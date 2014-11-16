dc0946d75c49bd89ea5483cb7394b9edfab34221
Yochannah Yehudi <yochannah@gmail.com>
2014-11-16 14:21:03 +0000

    campsign is generated behind the scenes, but not yet playable. Butt ugly too :)

--- script.js
+++ script.js
@@ -3,8 +3,12 @@ wh.dungeon = {
 	rawTiles:{},
 	current : {},
 	popup : function(room) {
-		var thedoc = window.open('','test','width=100,height=100,titlebar=0');
-		thedoc.document.write('BOOO');
+		//get template
+		var templateText = document.getElementById('cardTemplate').innerHTML;
+		
+	
+		var thedoc = window.open('','test','width=100,height=200,addressBar=0');
+		thedoc.document.write(templateText);
 	},
 	shownTiles : [],
 	readDungeon : function() {
@@ -19,17 +23,27 @@ wh.dungeon = {
 		wh.dungeon.rawTiles = $(xml);
 	},
 	getTileType : function (type) {
-		var tiles = wh.dungeon.rawTiles,
+		var tiles = $.extend(true,{},wh.dungeon.rawTiles),
 		items = $(tiles).find("tile").filter(function(){
               return $('type', this).text() == type;
         });		
-        console.log(items)
 		return items;
-		},
+	},
+	getAllButTileType : function (type) {
+		var tiles = $.extend(true,{},wh.dungeon.rawTiles),
+		items = $(tiles).find("tile").filter(function(){
+              return $('type', this).text() != type;
+        });		
+		return items;
+	},
+	getNumberOfRooms : function() {
+		return document.getElementById('numberOfRoomsInput').value;
+	},
 	setupObjectives : function() {
 		var objectiveList = document.getElementById('objectiveType'),
 		objs = wh.dungeon.getTileType('Objective'),
 		objHtml = "";
+		wh.dungeon.rawTiles.allObjectives = objs;
 		$.each(objs, function(i,v) {
 			objHtml += "<li>" + $(v).find('name').text() + "</li>";
 		});
@@ -38,19 +52,75 @@ wh.dungeon = {
 	},
 	handleObjectiveClicks : function() {
 		$(document.getElementById('objectiveType')).on('click','li', function(e) {
-			console.log('cleek');
 			wh.dungeon.setObjective(e.target.innerHTML);
+			//only highlight one at once.
+			if(document.querySelectorAll('.selected').length) {
+				document.querySelectorAll('.selected')[0].className="";
+			}
+			e.target.className = "selected";
+			wh.dungeon.handleGeneratorClicks();
 		});
+	},
+	handleGeneratorClicks : function() {
+		$(document.getElementById('generateDungeon')).click(function() {
+			wh.dungeon.current.numberOfTiles = wh.dungeon.getNumberOfRooms();
+			wh.dungeon.generateDungeon();
+		});
+	},
+	generateDungeon : function(){
+		//count non-dungeon tiles
+		var tiles = wh.dungeon.getAllButTileType('Objective'), nos, 
+		finalArray = [],
+		firstHalf = [],
+		secondHalf = [],
+		divisionIndex;
+		wh.dungeon.rawTiles.nonObjectives = nos = tiles;
+		//generate x random numbers / tile references
+				divisionIndex = wh.dungeon.getDivisionIndex();
+
+		for(var i = 0; i < nos.length;i++) {
+			finalArray.push(i);
+		}
+		
+		finalArray = wh.dungeon.shuffle(finalArray); 
+		finalArray = finalArray.slice(0,wh.dungeon.current.numberOfTiles-1);
+		firstHalf = finalArray.slice(0,divisionIndex);
+		secondHalf = finalArray.slice(divisionIndex);
+		secondHalf.push('Objective');
+		secondHalf = wh.dungeon.shuffle(secondHalf);
+		finalArray = firstHalf.concat(secondHalf);
+		wh.dungeon.current.order = finalArray;
 	},
 	setObjective : function(objective) {
 		wh.dungeon.current.objective = objective;
-		wh.dungeon.current.numberOfTiles = document.getElementById('numberOfRooms').value;
+	},
+	getDivisionIndex : function() {
+		divisor = 2;
+		divisionIndex = wh.dungeon.current.numberOfTiles / divisor;
+		return Math.ceil(divisionIndex);
 	},
 	init : function() {
 		wh.dungeon.readDungeon();
 		$(document).ajaxComplete(function() {
 			wh.dungeon.setupObjectives();
 		});
+	},
+	shuffle : function (array) { //http://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+		var currentIndex = array.length, temporaryValue, randomIndex ;
+
+  		// While there remain elements to shuffle...
+  		while (0 !== currentIndex) {
+
+	    	// Pick a remaining element...
+    		randomIndex = Math.floor(Math.random() * currentIndex);
+    		currentIndex -= 1;
+
+	    	// And swap it with the current element.
+    		temporaryValue = array[currentIndex];
+    		array[currentIndex] = array[randomIndex];
+    		array[randomIndex] = temporaryValue;
+		}
+  		return array;
 	}
 };
 
